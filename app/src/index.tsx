026c188a89fe8b2064056968ee65605fc6e3e61d
Tom Hurford <hurford.t0@gmail.com>
2023-03-04 23:53:00 +0000

    Updates and working on CI

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 
@@ -12,6 +12,10 @@ root.render(
         <App />
     </React.StrictMode>
 )
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
